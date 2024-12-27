2b1bf0e5754ee45179804765d86211bb72de2378
Utkarsh Sharma <sharmautkarsh804@gmail.com>
2024-12-27 20:29:48 +0530

    first commit

---
+++ src/main/java/com/utkarshprojects/salestrackingtool/SalesTrackingToolApplication.java
@@ -0,0 +1,13 @@
+package com.utkarshprojects.salestrackingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTrackingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesTrackingToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarshprojects/salestrackingtool/SalesTrackingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarshprojects.salestrackingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTrackingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
